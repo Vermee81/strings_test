266ad8aa3497a8d684f53a2973f9016fdf9f8860
Vermee81 <hrksb5029@gmail.com>
2019-09-30 01:38:35 +0900

    [update] setupとteardownを追加する

--- strings_test.go
+++ strings_test.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
+
+// setUpWithTeardown
+func setupWithTeardown() func() {
+	fmt.Println("セットアップ")
+	return func() {
+		fmt.Println("Tear down")
+	}
+}
 
 // TestToUpper はstringsのToUpperのテスト
 func TestToUpper(t *testing.T) {
@@ -14,16 +23,20 @@ func TestToUpper(t *testing.T) {
 		{"bar", "BAR"},
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.input, func(t *testing.T) {
-			t.Parallel()
-			result := strings.ToUpper(tt.input)
-			if tt.expected != result {
-				t.Errorf("expected %v, but %v", tt.expected, result)
-			}
-		})
-	}
+	teardown := setupWithTeardown()
+	t.Run("parent group", func(t *testing.T) {
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.input, func(t *testing.T) {
+				t.Parallel()
+				result := strings.ToUpper(tt.input)
+				if tt.expected != result {
+					t.Errorf("expected %v, but %v", tt.expected, result)
+				}
+			})
+		}
+	})
+	teardown()
 }
 
 // TestToLower はstringsのToLowerのテスト
@@ -35,14 +48,18 @@ func TestToLower(t *testing.T) {
 		{"BAR", "bar"},
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.input, func(t *testing.T) {
-			t.Parallel()
-			result := strings.ToLower(tt.input)
-			if tt.expected != result {
-				t.Errorf("expected %v, but %v", tt.expected, result)
-			}
-		})
-	}
+	teardown := setupWithTeardown()
+	t.Run("parent group", func(t *testing.T) {
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.input, func(t *testing.T) {
+				t.Parallel()
+				result := strings.ToLower(tt.input)
+				if tt.expected != result {
+					t.Errorf("expected %v, but %v", tt.expected, result)
+				}
+			})
+		}
+	})
+	teardown()
 }
