5766f97372aa068c29357e7fcc9cbf2e855a1f15
Vermee81 <hrksb5029@gmail.com>
2019-09-30 01:17:20 +0900

    :bug: ファイル名を変更する

--- strings_test.go
+++ strings_test.go
@@ -17,7 +17,29 @@ func TestToUpper(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
 			result := strings.ToUpper(tt.input)
+			if tt.expected != result {
+				t.Errorf("expected %v, but %v", tt.expected, result)
+			}
+		})
+	}
+}
+
+// TestToLower はstringsのToLowerのテスト
+func TestToLower(t *testing.T) {
+	tests := []struct {
+		input, expected string
+	}{
+		{"FOO", "foo"},
+		{"BAR", "bar"},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
+			result := strings.ToLower(tt.input)
 			if tt.expected != result {
 				t.Errorf("expected %v, but %v", tt.expected, result)
 			}
